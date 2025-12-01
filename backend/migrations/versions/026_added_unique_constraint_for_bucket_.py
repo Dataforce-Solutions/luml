f2421ddf8b0864f8f870f27bfbf07e11fb49d6f3
K-Krut <katiakrut448@gmail.com>
2025-12-01 17:03:14 +0200

    fix: corrected down_revision in 026 migration

--- backend/migrations/versions/026_added_unique_constraint_for_bucket_.py
+++ backend/migrations/versions/026_added_unique_constraint_for_bucket_.py
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "026"
-down_revision: str | None = "023"
+down_revision: str | None = "024"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
